a275ad534e650cf438a7372f6c0e7d44b0c13007
tfrana <30726742+tfrana@users.noreply.github.com>
2018-04-13 23:02:15 +0200

    Update run.sh
    Remove duplicate entries

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 # raspbian-trimmer
 # Simple script to remove "bloatware" from fresh installation of Raspbian Stretch
 
-sudo apt-get remove --purge bluej geany greenfoot nodered bluej geany greenfoot-ide node-red scratch* sense-emu-tools python3-thonny realvnc-vnc-server realvnc-vnc-viewer idle-python* -y
+sudo apt-get remove --purge bluej* geany greenfoot* nodered node-red scratch* sense-emu-tools python3-thonny realvnc-vnc-* idle-python* -y
 
 sudo rm -rf /etc/vnc ~/python_games BlueJ\ Projects/ Greenfoot\ Projects/ Scratch\ Projects/
 
